9bf75cd41c75be00a19de977d1d2598743b992bc
Navid Constantinou <navidcy@gmail.com>
2019-12-22 08:37:50 +1100

    Generalizes bench tests to work with Float32 arithmetic

--- benchFFT.jl
+++ benchFFT.jl
@@ -5,19 +5,21 @@ using Random: seed!
 seed!(1234) # for reproducibility
 
 Lx, Ly = 2π, 3π
-nx, ny = 512, 512
+nx, ny = 256, 256
+T, tolerance = Float64, 1e-12
+# T, tolerance = Float32, 1e-6
 
-gr =  TwoDGrid(nx, Lx, ny, Ly)
+gr =  TwoDGrid(nx, Lx, ny, Ly, T=T)
 
 # test function used in bench tests
-f = real(ifft(rand(nx, ny) + im*rand(nx, ny)))
+f = real(ifft(rand(T, (nx, ny)) + im*rand(T, (nx, ny))))
 fcomplex  = Complex.(f)
 
 # initialize empty arrays
-dfdx = zeros(nx, ny)
-dfdx_c = zeros(Complex{Float64}, (nx, ny))
- fh  = zeros(Complex{Float64}, (gr.nk, gr.nl))
- fhr = zeros(Complex{Float64}, (gr.nkr, gr.nl))
+dfdx = zeros(T, (nx, ny))
+dfdx_c = zeros(Complex{T}, (nx, ny))
+ fh  = zeros(Complex{T}, (gr.nk, gr.nl))
+ fhr = zeros(Complex{T}, (gr.nkr, gr.nl))
 
 # various ways of computing ∂f/∂x
 
@@ -35,8 +37,8 @@ end
 
 effort = FFTW.PATIENT
 FFTW.set_num_threads(Sys.CPU_THREADS)
-fftplan = plan_fft(Array{Float64, 2}(undef, nx, ny), flags=effort)
-rfftplan = plan_rfft(Array{Float64, 2}(undef, nx, ny), flags=effort)
+fftplan = plan_fft(Array{T, 2}(undef, nx, ny), flags=effort)
+rfftplan = plan_rfft(Array{T, 2}(undef, nx, ny), flags=effort)
 
 function dx_using_fftplan(f)
   fh = fftplan*f
@@ -53,9 +55,9 @@ function dx_using_rfftplan(f)
 end
 
 function dx_using_fftplan_mul(f)
-  mul!(fh, fftplan, fcomplex) #fftplan within mul! only works for complex-valued arreys
+  mul!(fh, fftplan, fcomplex) #fftplan within mul! only works if all arrays are complex-valued
   @. fh = im*gr.k * fh
-  ldiv!(dfdx_c, fftplan, fh)
+  ldiv!(dfdx_c, fftplan, fh) #fftplan within ldiv! only works if all arrays are complex-valued
   return real.(dfdx_c)
 end
 
@@ -73,9 +75,12 @@ dfdx4 = dx_using_rfftplan(f)
 dfdx5 = dx_using_fftplan_mul(f)
 dfdx6 = dx_using_rfftplan_mul(f)
 
-if  isapprox(dfdx1, dfdx2, rtol=1e-13) && isapprox(dfdx1, dfdx3, rtol=1e-13) && isapprox(dfdx1, dfdx4, rtol=1e-13) && isapprox(dfdx1, dfdx5, rtol=1e-13) && isapprox(dfdx1, dfdx6, rtol=1e-13) #make sure that all functions compute ∂f/∂x the same 
+if (isapprox(dfdx1, dfdx2, rtol=tolerance) && isapprox(dfdx1, dfdx3, rtol=tolerance) 
+    && isapprox(dfdx1, dfdx4, rtol=tolerance) && isapprox(dfdx1, dfdx5, rtol=tolerance) 
+    && isapprox(dfdx1, dfdx6, rtol=tolerance))
+ #make sure that all functions compute ∂f/∂x the same 
   
-  println("Performing bench tests with nx=", nx, " and ny=", ny, " grid-points")
+  println("Performing bench tests for 2D FFTs using nx=", nx, " and ny=", ny, " grid-points with ", T, " arithmetic.")
   println(" ")
   println("computing ∂f/∂x using fft")
   @btime dx_using_fft(f);
@@ -89,14 +94,11 @@ if  isapprox(dfdx1, dfdx2, rtol=1e-13) && isapprox(dfdx1, dfdx3, rtol=1e-13) && 
   println("computing ∂f/∂x using rfftplan")
   @btime dx_using_rfftplan(f);
   println(" ")
-  println("computing ∂f/∂x using fftplan + mul!")
+  println("computing ∂f/∂x using fftplan & mul!")
   @btime dx_using_fftplan_mul(f);
   println(" ")
-  println("computing ∂f/∂x using rfftplan + mul!")
+  println("computing ∂f/∂x using rfftplan & mul!")
   @btime dx_using_rfftplan_mul(f);
 else
-  error("something went wrong with computing the derivatives")
+  error("something went wrong while computing the derivatives")
 end
-  
-  
-
